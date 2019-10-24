0f93feade86aebdbc6e2c093335ddaf589d7fa2e
tokyohuang123 <tokyohuang123@163.com>
2019-10-24 23:28:28 +0800

    增加了牧师

--- src/TestHero.java
+++ src/TestHero.java
@@ -51,53 +51,56 @@ class Hero {
 
     public void sexInfo(int sex) {
         if (sex == 1) {
-            System.out.println("这是一个男");
+            System.out.println("男");
         } else {
-            System.out.println("这是一个女");
+            System.out.println("女");
         }
 
     }
 
     public void btInfo(int at, int df, int hp) {
         if (at / df < 0.5 && hp > at) {
-            System.out.println("这是一个防御型的英雄");
+            System.out.println("防御型英雄");
         } else if (at / df > 1 && at / df < 2) {
-            System.out.println("这是一个攻守相对平衡的英雄");
+            System.out.println("平衡英雄");
         } else if (at / df > 2) {
-            System.out.println("这是一个极度攻击英雄");
+            System.out.println("攻击英雄");
         } else
-            System.out.println("这是一个攻守平衡的英雄");
+            System.out.println("攻守平衡英雄");
     }
 
     public void hpInfo(int hp) {
         if (hp == 0) {
-            System.out.println("这个英雄已经死亡");
+            System.out.println("英雄已经死亡");
         } else if (hp <= 10) {
-            System.out.println("这个英雄濒临死亡");
+            System.out.println("英雄濒临死亡");
         } else if (hp <= 20) {
-            System.out.println("这个英雄身中重伤");
+            System.out.println("身中重伤");
         } else if (hp <= 30) {
-            System.out.println("这个英雄身伤有伤，严重");
+            System.out.println("身伤有伤，严重");
         } else if (hp <= 40) {
-            System.out.println("这个英雄身伤有伤，比较严重");
+            System.out.println("身伤有伤，比较严重");
         } else if (hp <= 50) {
-            System.out.println("这个英雄身伤有伤");
+            System.out.println("身伤有伤");
         } else if (hp <= 60) {
-            System.out.println("这个英雄身伤有旧伤");
+            System.out.println("这身有旧伤");
         } else if (hp <= 70) {
-            System.out.println("这个英雄身体不好");
+            System.out.println("身体不好");
         } else if (hp <= 80) {
-            System.out.println("这个英雄身体一般");
+            System.out.println("身体一般");
         } else if (hp <= 90) {
-            System.out.println("这个英雄身体还行");
+            System.out.println("身体还行");
         } else if (hp <= 100) {
-            System.out.println("这个英雄身体健康");
+            System.out.println("身体健康");
         } else if (hp <= 120) {
-            System.out.println("这个英雄体质很好");
+            System.out.println("体质很好");
         } else if (hp <= 150) {
-            System.out.println("这个英雄极其强壮");
-        } else if (hp <= 190) {
-            System.out.println("这个英雄是超人");
+            System.out.println("极其强壮");
+        } else if (hp < 190) {
+            System.out.println("超人体质");
+        }
+        if (hp >= 190){
+            System.out.println("神体质");
         }
     }
 
@@ -154,6 +157,10 @@ class Zs extends Hero {
         return z;
 
     }
+    private Zs() {
+
+        System.out.println("你创建了一个战士 : ");
+    }
 
 
 
@@ -179,18 +186,38 @@ class Zs extends Hero {
 
     }
 
+class Ms extends Hero {
+    static Ms m;
+
+
+    public static Ms creatMs() {
+        if (m == null) {
+            Ms m = new Ms();
+            return m;
+        }
+        return m;
+
+    }
+
+    private Ms() {
+
+        System.out.println("你创建了一个牧师 : ");
+    }
+
+
+}
+
+    /**
+     * @author star
+     */
     public  class TestHero {
 
 
-        static int herocount ;
+        private static int herocount ;
 
 
         public static void main(String[] args) {
-            Random chose = new Random();
-            int ch = chose.nextInt(3);
-            String[] hero = {"战士", "法师", "牧师"};
-            System.out.println("你 随机到了" + hero[ch]);
-            int hc = herocount++;
+
             System.out.println("请指定英雄能力范围，本系统会随机生成一个英雄的属性");
             Scanner input = new Scanner(System.in);
             int setline = input.nextInt();
@@ -225,6 +252,24 @@ class Zs extends Hero {
             p2.setHero(" 三娃", 7, 1, "空手", "喷火");
             p2.setBt(a1, d1, h1,m1);
             p2.heroInfo();
+
+
+            int a2 = r.nextInt(setline);
+            int d2 = r.nextInt(setline);
+            int h2 = r.nextInt(setline);
+            int m2 = r.nextInt(setline);
+
+            Ms p3 = Ms.creatMs();
+
+            p3.setHero("七娃", 7, 1, "葫芦", "法宝");
+            p3.setBt(a2, d2, h2,m2);
+            p3.heroInfo();
+
+            Random chose = new Random();
+            int ch = chose.nextInt(3);
+            String[] hero = {"战士", "法师", "牧师"};
+            System.out.println("你 随机到了" + hero[ch]);
+            int hc = herocount++;
             System.out.println("目前有 " + hc + " 个英雄 ");
 
 
